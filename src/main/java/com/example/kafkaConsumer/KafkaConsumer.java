32df6a25b1a4800dc4e4a7ad3f53ccb28db352fd
xxueewa <xueyanw@utexas.edu>
2024-02-11 13:55:06 -0800

    init

---
+++ src/main/java/com/example/kafkaConsumer/KafkaConsumer.java
@@ -0,0 +1,12 @@
+package com.example.kafkaConsumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    @KafkaListener(topics = "test-topic", groupId = "test-group")
+    public void consumer(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
---
+++ src/main/java/com/example/kafkaConsumer/KafkaController.java
@@ -0,0 +1,19 @@
+package com.example.kafkaConsumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping("/send")
+    public void sendMessage(@RequestBody String message) {
+        kafkaTemplate.send("test-topic", message);
+    }
+}
